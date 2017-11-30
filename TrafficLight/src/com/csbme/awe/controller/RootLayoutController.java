1833ddf225f2c9d375a46015d2319614410299a6
csbme <seb.stucke@ymail.com>
2017-11-30 21:48:10 +0100

    show icon in alert window

--- TrafficLight/src/com/csbme/awe/controller/RootLayoutController.java
+++ TrafficLight/src/com/csbme/awe/controller/RootLayoutController.java
@@ -3,6 +3,8 @@ package com.csbme.awe.controller;
 import com.csbme.awe.Main;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
 
 /**
  * The type Root layout controller.
@@ -41,6 +43,9 @@ public class RootLayoutController {
         alert.setTitle("Traffic Light Simulator 3000 'Final Apex Elite'-Edition");
         alert.setHeaderText("About");
         alert.setContentText("\"You don’t need to go to church to be a Christian. If you go to Taco Bell, that doesn’t make you a taco.\"\n\n- Justin Bieber");
+
+        Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
+        stage.getIcons().add(new Image("images/Java.png"));
 
         alert.showAndWait();
     }
